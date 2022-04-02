7eb02ac189f135b648ea1dba7045a15f82efa563
Danil Ostaptsov <dost4657@gmail.com>
2022-04-02 12:27:26 +0300

    dge

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
+import React, { useState } from 'react';
+import { Text, TextInput, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import translate from './translator';
 
-export default function App() {
+export default function App(){
+  const [text, setText] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <SafeAreaView style={{padding: 10, paddingTop: 80, flex: 1}}>
+     <StatusBar style={"auto"}/>
+    <Text style = {{fontWeight: "bold", fontSize: 60, paddingBottom: 20}}>
+      Emoji Translator
+    </Text>
+    <Text style = {{fontSize: 32, fontWeight: "bold"}}>Введите текст</Text>
+    <TextInput 
+      style = {{height: 40}}
+      onChangeText = {setText}
+      defaultValue = {text}
+      placeholder = {"Ваш текст"}/>
+
+    <Text style = {{fontSize: 42}}>
+      {translate(text) || "..."}
+    </Text>
+   </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
