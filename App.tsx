180f0d88d9cbe90611dd6a2d027e7bdbac4aac33
Danil Ostaptsov <dost4657@gmail.com>
2022-04-09 15:57:28 +0300

    visual

--- App.tsx
+++ App.tsx
@@ -1,27 +1,67 @@
 import React, { useState } from 'react';
-import { Text, TextInput, SafeAreaView } from 'react-native';
+import { Text, TextInput, SafeAreaView, StyleSheet, ScrollView, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import translate from './translator';
+import { useFonts, ComicNeue_400Regular } from '@expo-google-fonts/comic-neue';
 
 export default function App(){
   const [text, setText] = useState('');
-
+  const [fontsLoaded] = useFonts({ ComicNeue_400Regular });
+  if (!fontsLoaded) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Loading fonts...</Text>
+      </SafeAreaView>
+    );
+  }
   return (
-   <SafeAreaView style={{padding: 10, paddingTop: 80, flex: 1}}>
+   <SafeAreaView style={{padding: 10, paddingTop: 30, flex:10}}>
      <StatusBar style={"auto"}/>
-    <Text style = {{fontWeight: "bold", fontSize: 60, paddingBottom: 20}}>
-      Emoji Translator
-    </Text>
-    <Text style = {{fontSize: 32, fontWeight: "bold"}}>Введите текст</Text>
-    <TextInput 
-      style = {{height: 40}}
-      onChangeText = {setText}
-      defaultValue = {text}
-      placeholder = {"Ваш текст"}/>
-
-    <Text style = {{fontSize: 42}}>
-      {translate(text) || "..."}
-    </Text>
+     <SafeAreaView>
+      <Text style={styles.header}>
+        {'Emoji\nTranslator'}
+      </Text>
+    </SafeAreaView>
+    <View style = {styles.translator}>
+      <ScrollView>
+        <TextInput 
+          style = {{ fontSize: 32}}
+          onChangeText = {setText}
+          defaultValue = {text}
+          placeholder = {"Вы думали, что это ваш текст,но на самом деле это я, Дио!"}
+          multiline = {true}
+          textAlignVertical = "top"
+        />
+      </ScrollView>
+    </View>
+    <View style = {styles.translator}>
+      <ScrollView>
+        <Text style = {{fontSize: 42}}>
+          {translate(text) || ""}
+        </Text>
+      </ScrollView>
+    </View>
    </SafeAreaView>
   );
 }
+
+
+  const styles = StyleSheet.create({
+   header: {
+      fontFamily: 'ComicNeue_400Regular',
+      fontSize: 50,
+      paddingBottom: 20,
+      textShadowColor: '#30d5c8',
+      textShadowRadius: 10
+    },
+    container: {
+      padding: 10,
+      paddingTop: 30,
+    }, 
+    translator: {
+      flex: 45,
+      borderWidth: 3,
+      borderRadius: 20,
+      borderColor: '#30d5c8'
+    }
+  })
